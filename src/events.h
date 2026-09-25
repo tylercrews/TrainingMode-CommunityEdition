daa5ad425c146c46f09f9e556ff9828f870224a4
tylercrews <tycrews64@gmail.com>
2026-09-25 01:02:28 -0400

    changin version number for my personal branch

--- src/events.h
+++ src/events.h
@@ -6,8 +6,8 @@
 #include "savestate.h"
 #include <stdint.h>
 
-#define TM_VERSSHORT "TM-CE v1.4 d1"
-#define TM_VERSLONG "TM Community Edition v1.4 d1"
+#define TM_VERSSHORT "TM-Tyro v1.1"
+#define TM_VERSLONG "TM Tyro Edition v1.1"
 #define EVENT_DATASIZE 512
 #define TM_FUNC -(50 * 4)
 
